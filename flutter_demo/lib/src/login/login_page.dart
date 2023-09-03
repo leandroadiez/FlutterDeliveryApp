65e46b76b0ef534ad0dc032e1e3eb0c89f74714b
leandroadiez <leandroadiez@gmail.com>
2023-09-03 19:35:53 -0300

    login page v2

--- flutter_demo/lib/src/login/login_page.dart
+++ flutter_demo/lib/src/login/login_page.dart
@@ -14,46 +14,140 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Container(
         width: double.infinity,
-        child: Column(
+        child: Stack(
           children: [
-            Image.asset('assets/img/delivery.png',
-            width: 200,
-            height: 200),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Username',
-              ),
+            Positioned(
+                top: -80,
+                left: -100,
+                child: _circleLogin()),
+            Positioned(
+                child: _textLogin(),
+                top: 60,
+                left: 25,
             ),
-            const TextField(
-                decoration: InputDecoration(
-                    hintText: 'Contraseña',
-                ),
-            ),
-            ElevatedButton(
-                onPressed: (){},
-                child: Text('Ingresar')),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
               children: [
-                Text('No tienes cuenta?',
-                style: TextStyle(
-                  color: MyColors.primaryColor
-                )),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                    'Registrate',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: MyColors.primaryColor
-                    )
-                )
+                _imageBanner(),
+                _textFieldUsername(),
+                _textFieldPassword(),
+                _buttonLogin(),
+                _textDontHaveAccount(),
               ],
-            )
+            ),
           ],
         ),
       )
+    );
+  }
+  Widget _textLogin () {
+    return Text (
+      'LOGIN',
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 22,
+      )
+    );
+  }
+
+  Widget _circleLogin () {
+    return Container(
+      width: 240,
+      height: 230,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: MyColors.primaryColor,
+      ),
+    );
+  }
+  
+  Widget _imageBanner () {
+    return Container(
+      margin: EdgeInsets.only(
+          top: 100,
+          bottom: MediaQuery.of(context).size.height * 0.2),
+      child: Image.asset('assets/img/delivery.png',
+          width: 200,
+          height: 200),
+    );
+  }
+  Widget _textFieldUsername (){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Username',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(20),
+          hintStyle: TextStyle(
+            color: MyColors.primaryColorDark,
+          ),
+          prefixIcon: Icon(Icons.email, color: MyColors.primaryColor),
+        ),
+      ),
+    );
+  }
+  Widget _textFieldPassword () {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Password',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(20),
+          hintStyle: TextStyle(
+            color: MyColors.primaryColorDark,
+          ),
+          prefixIcon: Icon(Icons.lock, color: MyColors.primaryColor),
+        ),
+      ),
+    );
+  }
+  Widget _buttonLogin () {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+      child: ElevatedButton(
+          onPressed: (){},
+          child: const Text('Ingresar'),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: MyColors.primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15)
+      )),
+    );
+  }
+  Widget _textDontHaveAccount(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('No tienes cuenta?',
+            style: TextStyle(
+                color: MyColors.primaryColor
+            )),
+        SizedBox(
+          width: 5,
+        ),
+        Text(
+            'Registrate',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: MyColors.primaryColor
+            )
+        )
+      ],
     );
   }
 }
--- flutter_demo/lib/src/utils/my_colors.dart
+++ flutter_demo/lib/src/utils/my_colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 
 class MyColors {
   static Color primaryColor = const Color(0xFF6200EA);
-
+  static Color primaryColorDark = const Color(0xFF420098);
+  static Color primaryOpacityColor = const Color.fromRGBO(119, 42, 218, 0.12);
 }
