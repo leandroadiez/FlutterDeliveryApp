c95873e220b4865dfc3a52a781734efa8663cdff
leandroadiez <leandroadiez@gmail.com>
2023-09-03 16:24:21 -0300

    initial commit

---
+++ flutter_demo/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/src/login/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Delivery App Flutter',
+        initialRoute: 'login',
+        routes: {'login': (BuildContext context) => const LoginPage()});
+  }
+}
---
+++ flutter_demo/lib/src/login/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Delivery App'),
+
+      ),
+      body: Stack(children: [
+        Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(135)),
+                color: Colors.lime
+            ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 60, left: 50),
+          child: Text('LOGIN'),
+        ),
+      ],)
+    );
+  }
+}
